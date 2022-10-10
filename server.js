ddf1e8f71147e74e1eebbce63e477f4b86b2d28d
Kanishka Samarathunga <kanishkakcs@gmail.com>
2022-10-11 00:49:13 +0530

    CORS re-added

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+app.use(cors({
+  origin: "https://dev-kani-future-goals-mern-app.netlify.app",
+  credentials: true
+}))
+
 app.use('/api/goals', require('./routes/goalRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
 
