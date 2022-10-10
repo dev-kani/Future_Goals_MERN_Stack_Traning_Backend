ef62efc2c451d8202ed4a2b83cb287cbb98dc3d9
Kanishka Samarathunga <kanishkakcs@gmail.com>
2022-10-10 21:53:30 +0530

    CORS

--- server.js
+++ server.js
@@ -6,16 +6,41 @@ const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
 
-connectDB()
-
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+// CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+
+  next();
+})
+
 app.use('/api/goals', require('./routes/goalRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
 
+// connect to frontend
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+//   app.get('*', (req, res) =>
+//     res.sendFile(
+//       path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+//     )
+//   );
+// } else {
+//   app.get('/', (req, res) => res.send('Please set to production'));
+// }
+
 app.use(errorHandler)
+
+connectDB()
 
 app.listen(port, () => console.log(`Server running ${port}`))
