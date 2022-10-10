eaa7e14704b89bffa726cbb1abca2edb7d2b4dcd
Kanishka Samarathunga <kanishkakcs@gmail.com>
2022-10-11 01:17:11 +0530

    CORS added

--- server.js
+++ server.js
@@ -11,10 +11,17 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.use(cors({
-  origin: "https://dev-kani-future-goals-mern-app.netlify.app",
-  credentials: true
-}))
+// CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+
+  next();
+})
 
 app.use('/api/goals', require('./routes/goalRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
