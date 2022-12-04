47221086d2b4c47fab334e7f81fb1dbd1f11dbc1
Kanishka Samarathunga <kanishkakcs@gmail.com>
2022-12-04 06:41:49 +0530

    Update goalRoutes.js

--- routes/goalRoutes.js
+++ routes/goalRoutes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const { getGoals, setGoals, updateGoals, deleteGoals } = require('../controllers/goalController')
 
-const { protect } = require('../middleware/authMiddleware')
+const { protect } = require('../middleware/authMiddleware');
 
 router.route('/').get(protect, getGoals).post(protect, setGoals)
 router.route('/:id').delete(protect, deleteGoals).put(protect, updateGoals)
